3c9f78b8e0180f00522614dd0825a84b76c7affe
Christopher Oka <christopheroka@v1040-wn-rt-a-17-20.campus-dynamic.uwaterloo.ca>
2023-07-19 12:26:10 -0400

    added time tracking so every 15 minutes the angle updates

--- Prototype/Prototype.ino
+++ Prototype/Prototype.ino
@@ -15,7 +15,7 @@ Adafruit_INA219 ina219;
 AccelStepper stepper(AccelStepper::FULL4WIRE, motorPin1, motorPin3, motorPin2, motorPin4);
 int startingPosition = 0;
 double optimal_angles[] = {-90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -87.6633278353471, -84.2661106613641, -80.89733310920033, -77.55156563727671, -74.2236607898597, -70.908772185975, -67.60237065826973, -64.30025872351993, -60.99858421897897, -57.693853622290945, -54.38294528340668, -51.063122531748185, -47.73204637931164, -44.3877873216574, -41.028835547780254, -37.65410871315806, -34.2629563169917, -30.85515966487738, -27.430926402477485, -23.990878682941208, -20.53603418601884, -17.067779440263894, -13.587835203892537, -10.098214020377517, -6.60117045881347, -3.0991449477908497, 0.4052975174854311, 3.909532204971062, 7.410941201753367, 10.90697977605977, 14.395240637342287, 17.87351431941664, 21.339844152151954, 24.7925746118845, 28.230392219829643, 31.652358557800255, 35.057935359821776, 38.44700198877301, 41.81986589787404, 45.17726689462972, 48.520376163877806, 51.85079106807788, 55.17052673324636, 58.48200535685959, 61.788044050574136, 65.09184186552835, 68.39696644999962, 71.70734056452415, 75.02722843128566, 78.36122162384815, 81.71422390792914, 85.09143412121767, 88.49832582633016, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90};
-const int stepsPerRevolution = 2048; 
+const int stepsPerRevolution = 2038; 
 
 
 // SENSOR INITIAL SETUP
@@ -34,8 +34,11 @@ float t = 0;
 int led = 2000;
 bool state = false;
 
-int hour = 10;
-int minute = 50;
+int initial_minute = 50;
+int elapsed_hour = 0;
+int elapsed_minute = 0;
+int current_hour = 0;
+int current_minute = 0;
 
 void setup()
 {
@@ -93,9 +96,6 @@ void setup()
 //  Serial.println("Thank you! Now calibrating...");
 //  Serial.println(hour, minute);
 
-  int initial_angle_index = (hour * 4) + (minute / 15);
-  int initial_angle = optimal_angles[initial_angle_index];
-  int initial_steps = initial_angle * stepsPerRevolution / 360;
   stepper.moveTo(initial_steps);
   delay(10*1000);
 }
@@ -103,8 +103,14 @@ void setup()
 void loop()
 { 
   
+  elapsed_hour = round(t / 3600);
+  elapsed_minute = ((t / 3600 - round(t/3600)) * 60);
+  current_hour = initial_hour + elapsed_hour;
+  current_minute = initial_minute + elapsed_minute;
+
+  
   // STEPPER MOTOR OPERATION
-  int angle_index = (hour * 4) + (minute / 15);
+  int angle_index = (current_hour * 4) + (current_minute / 15);
   int optimal_angle = optimal_angles[angle_index];
 
   // Calculate the difference between the previous optimal angle and the new one
@@ -147,6 +153,7 @@ void loop()
     power_mW = (0.7 * voltage) * current_mA;
 
     // Format: Time, Voltage, Current, Estimated Power
+    Serial.print(t)
     Serial.print(", ");
     Serial.print(voltage);
     Serial.print(", ");
