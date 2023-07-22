a68af15ef9c9b152cf4b2aa728a65098b7102fd6
ansonyuu <ansonyu24@gmail.com>
2023-07-21 20:53:00 -0400

    fix: added comments

--- Prototype/Prototype.ino
+++ Prototype/Prototype.ino
@@ -1,34 +1,32 @@
-//TODO: Global clock system using millis
+// 0. INSTALL PACKAGES
+#include <AccelStepper.h> // Package for programming stepper motor
+#include <Wire.h> // Package for communicating through SDA and SCL
+#include <Adafruit_INA219.h> // Package for programming INA219
 
-#include <AccelStepper.h>
-#include <Wire.h>
-#include <Adafruit_INA219.h>
+// 1. DEFINITIONS
+#define motorPin1 14 // Motor pin for stepper motor
+#define motorPin2 27 // Motor pin for stepper motor
+#define motorPin3 26 // Motor pin for stepper motor
+#define motorPin4 25 // Motor pin for stepper motor
 
-#define motorPin1 14
-#define motorPin2 27
-#define motorPin3 26
-#define motorPin4 25
-
-Adafruit_INA219 ina219;
-
-// STEPPER INITIAL SETUP
-AccelStepper stepper(AccelStepper::FULL4WIRE, motorPin1, motorPin3, motorPin2, motorPin4);
-int startingPosition = 0;
-double optimal_angles[] = {-90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -87.6633278353471, -84.2661106613641, -80.89733310920033, -77.55156563727671, -74.2236607898597, -70.908772185975, -67.60237065826973, -64.30025872351993, -60.99858421897897, -57.693853622290945, -54.38294528340668, -51.063122531748185, -47.73204637931164, -44.3877873216574, -41.028835547780254, -37.65410871315806, -34.2629563169917, -30.85515966487738, -27.430926402477485, -23.990878682941208, -20.53603418601884, -17.067779440263894, -13.587835203892537, -10.098214020377517, -6.60117045881347, -3.0991449477908497, 0.4052975174854311, 3.909532204971062, 7.410941201753367, 10.90697977605977, 14.395240637342287, 17.87351431941664, 21.339844152151954, 24.7925746118845, 28.230392219829643, 31.652358557800255, 35.057935359821776, 38.44700198877301, 41.81986589787404, 45.17726689462972, 48.520376163877806, 51.85079106807788, 55.17052673324636, 58.48200535685959, 61.788044050574136, 65.09184186552835, 68.39696644999962, 71.70734056452415, 75.02722843128566, 78.36122162384815, 81.71422390792914, 85.09143412121767, 88.49832582633016, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90};
-const int stepsPerRevolution = 2038; 
+// Motor Setup
+AccelStepper stepper(AccelStepper::FULL4WIRE, motorPin1, motorPin3, motorPin2, motorPin4); // Instantiating stepper motor
+int startingPosition = 0; 
+double optimal_angles[] = {-90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -90, -87.6633278353471, -84.2661106613641, -80.89733310920033, -77.55156563727671, -74.2236607898597, -70.908772185975, -67.60237065826973, -64.30025872351993, -60.99858421897897, -57.693853622290945, -54.38294528340668, -51.063122531748185, -47.73204637931164, -44.3877873216574, -41.028835547780254, -37.65410871315806, -34.2629563169917, -30.85515966487738, -27.430926402477485, -23.990878682941208, -20.53603418601884, -17.067779440263894, -13.587835203892537, -10.098214020377517, -6.60117045881347, -3.0991449477908497, 0.4052975174854311, 3.909532204971062, 7.410941201753367, 10.90697977605977, 14.395240637342287, 17.87351431941664, 21.339844152151954, 24.7925746118845, 28.230392219829643, 31.652358557800255, 35.057935359821776, 38.44700198877301, 41.81986589787404, 45.17726689462972, 48.520376163877806, 51.85079106807788, 55.17052673324636, 58.48200535685959, 61.788044050574136, 65.09184186552835, 68.39696644999962, 71.70734056452415, 75.02722843128566, 78.36122162384815, 81.71422390792914, 85.09143412121767, 88.49832582633016, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90}; // Optimal angles for Waterloo, Canada on July 20th, 2023. Calculated using NREL data and sun.py
+const int stepsPerRevolution = 2038; // Steps in 24BYJ48S stepper motor
 const int stepsPerDegree = round(2038 / 360);
 
 
-// SENSOR INITIAL SETUP
-int base = 33;   // Define pins
+// Current Sensor Setup
+Adafruit_INA219 ina219; // Instantiating INA219
+int base = 33;   // Define base pins
 float freq = 2; // Reporting frequency Hz
 int del = 2;    // Delay after changing state of transistor
-
-float current_mA = 0; // Sensor variables
+float current_mA = 0; // 
 float voltage = 0;
 float power_mW = 0;
 
-// TIME TRACKING INITIAL SETUP
+// Time Tracking Setup
 unsigned long last = 0;
 unsigned long led_last = 0;
 float t = 0;
@@ -42,42 +40,33 @@ int elapsed_minute = 0;
 int current_hour = 0;
 int current_minute = 0;
 
-
+// Initial position setup
 bool checked = false;
-
 
 void setup()
 {
-  delay(5000);
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  // 2. Calibrating devices
+  delay(5000); // 5 second delay after setup to allow user to move panel to correct location
+  Serial.begin(115200); // Defining serial rate
 
-  // STEPPER MOTOR SETUP
-  startingPosition = stepper.currentPosition();
-
-    int initial_angle_index = (initial_hour * 4) + (initial_minute / 15);
-    int initial_angle = optimal_angles[initial_angle_index];
-    int initial_steps = round(initial_angle * stepsPerDegree);
-
-    // move stepper to inital angle
-    stepper.moveTo(initial_steps);
+  // Stepper motor calibration
+  startingPosition = stepper.currentPosition(); // Storing current position of stepper motor
+  int initial_angle_index = (initial_hour * 4) + (initial_minute / 15); // Obtaining index for optimal angle based on time
+  int initial_angle = optimal_angles[initial_angle_index]; // Ontaining angle from calculated index
+  int initial_steps = round(initial_angle * stepsPerDegree);
+  stepper.moveTo(initial_steps); // Move stepper to inital angle
 
   while (!Serial)
   {
-    // will pause Zero, Leonardo, etc until serial console opens
-    delay(1);
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
   }
 
-  uint32_t currentFrequency;
+  uint32_t currentFrequency; // Define variable for current frequency
+  Serial.println("Hello!"); // Indicate power
 
-  Serial.println("Hello!");
-
-  // Initialize the INA219.
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
-  if (!ina219.begin())
+  if (!ina219.begin())   // Initialize the INA219 to default, largest range (32V, 2A)
   {
-    Serial.println("Failed to find INA219 chip");
+    Serial.println("Failed to find INA219 chip"); 
     while (1)
     {
       delay(10);
@@ -89,8 +78,7 @@ void setup()
   ina219.setCalibration_16V_400mA();
 //
   Serial.println("Measuring voltage and current with INA219 ...");
-//
-//  // SENSOR SETUP
+  // SENSOR SETUP
   pinMode(base, OUTPUT);
 
   last = millis();
